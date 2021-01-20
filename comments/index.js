d92b8d1c14052d8e30d7b5db1d9ec4f8564455e6
Ken Leung <kenleung5e28@gmail.com>
2021-01-20 14:14:20 +0800

    fix typos

--- comments/index.js
+++ comments/index.js
@@ -42,20 +42,20 @@ app.post('/events', async (req, res) => {
 
   const { type, data } = req.body;
   if (type === 'CommentModerated') {
-    const { id, postId, status } = data;
+    const { id, postId, content, status } = data;
     const comments = commentsByPostId[postId];
     const comment = comments.find(comment => comment.id === id);
     comment.status = status;
 
     await axios.post('http://localhost:4005/events', {
-    type: 'CommentUpdated',
-    data: {
-      id,
-      content,
-      status,
-      postId
-    }
-  });
+      type: 'CommentUpdated',
+      data: {
+        id,
+        content,
+        status,
+        postId
+      }
+    });
   }
 
   res.send({});
